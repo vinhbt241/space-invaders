3f991c96a62ac4ec15b963aa428be13cc23042cc
Vinh <vinhbuith32412000@gmail.com>
2026-07-19 21:10:53 +0700

    add projectile pool

--- script.js
+++ script.js
@@ -17,13 +17,49 @@ class Player {
     if(this.game.keys.indexOf('ArrowRight') > - 1) this.x += this.speed
 
     // boundaries
-    if(this.x < 0) this.x = 0
-    else if(this.x > this.game.width - this.width) this.x = this.game.width - this.width
+    if(this.x < -this.width * 0.5) this.x = -this.width * 0.5
+    else if(this.x > this.game.width - this.width * 0.5) this.x = this.game.width - this.width * 0.5
+  }
+
+  shoot() {
+    const projectile = this.game.getProjectile()
+    if(projectile) projectile.start(this.x + this.width * 0.5, this.y)
   }
 }
 
 class Projectile {
+  constructor() {
+    this.width = 8
+    this.height = 20
+    this.x = 0
+    this.y = 0
+    this.speed = 20
+    this.free = true
+  }
 
+  draw(context) {
+    if (!this.free) {
+      context.fillRect(this.x, this.y, this.width, this.height)
+    }
+  }
+
+  update() {
+    if(!this.free) {
+      this.y -= this.speed
+
+      if(this.y < -this.height) this.reset()
+    }
+  }
+
+  start(x, y) {
+    this.x = x
+    this.y = y
+    this.free = false
+  }
+
+  reset() {
+    this.free = true
+  }
 }
 
 class Enemy {
@@ -38,25 +74,42 @@ class Game {
     this.keys = []
     this.player = new Player(this)
 
+    this.projectilesPool = []
+    this.numberOfProjectiles = 10
+    this.createProjectiles();
+
     // event listeners
     window.addEventListener('keydown', e => {
-      if (this.keys.indexOf(e.key) === -1) {
-        this.keys.push(e.key)
-      }
+      if(this.keys.indexOf(e.key) === -1) this.keys.push(e.key)
+      if(e.key === '1') this.player.shoot()
     })
 
     window.addEventListener('keyup', e => {
       const index = this.keys.indexOf(e.key)
 
-      if (index > -1) {
-        this.keys.splice(index, 1);
-      }
+      if(index > -1) this.keys.splice(index, 1);
     })
   }
 
   render(context) {
     this.player.draw(context)
     this.player.update()
+    this.projectilesPool.forEach(projectile => {
+      projectile.update()
+      projectile.draw(context)
+    })
+  }
+
+  createProjectiles() {
+    for(let i = 0; i < this.numberOfProjectiles; i++) {
+      this.projectilesPool.push(new Projectile())
+    }
+  }
+
+  getProjectile() {
+    for(let i = 0; i < this.numberOfProjectiles; i++) {
+      if(this.projectilesPool[i].free) return this.projectilesPool[i]
+    }
   }
 }
 
